ab3eaaf340158ead6b68c33e25bf89ea3f614a06
Bart <bart@barts-mbp.home>
2024-11-25 16:20:48 -0500

    feat: Added onestate tracker

--- app/root.tsx
+++ app/root.tsx
@@ -1,36 +1,21 @@
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-    Link
-} from "@remix-run/react";
-import type {LinksFunction} from "@remix-run/node";
+import { Link, Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
-import "./tailwind.css";
+import './tailwind.css';
 
-export const links: LinksFunction = () => [
-    {rel: "preconnect", href: "https://fonts.googleapis.com"},
-    {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossOrigin: "anonymous",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    },
-];
 
-export function Layout({children}: { children: React.ReactNode }) {
+export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <Meta/>
-            <Links/>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <script
+                defer
+                data-site-id="utils.inthefield.me"
+                src="https://assets.onedollarstats.com/tracker.js">
+            </script>
+            <Meta />
+            <Links />
         </head>
         <body>
         <nav className="p-2 bg-gray-100 dark:bg-gray-800 ">
@@ -43,13 +28,13 @@ export function Layout({children}: { children: React.ReactNode }) {
             </div>
         </nav>
         {children}
-        <ScrollRestoration/>
-        <Scripts/>
+        <ScrollRestoration />
+        <Scripts />
         </body>
         </html>
     );
 }
 
 export default function App() {
-    return <Outlet/>;
+    return <Outlet />;
 }
